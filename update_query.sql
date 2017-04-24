a8e87a3e79d4f210abd9d5b69598ca80f361f28a
Andy <jey.developer@gmail.com>
2017-04-25 02:22:32 +0700

    update sql file

--- update_query.sql
+++ update_query.sql
@@ -11,3 +11,17 @@ ALTER TABLE `abs_bagian`
 
 ALTER TABLE `abs_bagian`
   MODIFY `bag_id` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE `abs_status` (
+  `sta_id` int(11) NOT NULL,
+  `sta_name` varchar(100) NOT NULL,
+  `sta_void` tinyint(4) NOT NULL,
+  `sta_created_at` datetime NOT NULL,
+  `sta_updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `abs_status`
+  ADD PRIMARY KEY (`sta_id`);
+
+ALTER TABLE `abs_status`
+  MODIFY `sta_id` int(11) NOT NULL AUTO_INCREMENT;
