7f311552c983353648cfe4173f6b284e282024a9
jeydeveloper <jey.developer@gmail.com>
2017-07-12 20:45:03 +0700

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -30,6 +30,78 @@ return [
 
         'determineRouteBeforeAppMiddleware' => true,
 
-        'baseUrl'        => "http://" . $_SERVER['HTTP_HOST'] . preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])) . "/"
+        'baseUrl'        => "http://" . $_SERVER['HTTP_HOST'] . preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])) . "/",
+
+        'dataStatic' => [
+          'religion' => [
+            [
+              'key' => 'islam',
+              'value' => 'Islam',
+            ],
+            [
+              'key' => 'kristen',
+              'value' => 'Kristen',
+            ],
+            [
+              'key' => 'protestan',
+              'value' => 'Protestan',
+            ],
+            [
+              'key' => 'budha',
+              'value' => 'Budha',
+            ],
+            [
+              'key' => 'hindu',
+              'value' => 'Hindu',
+            ],
+          ],
+          'gender' => [
+            [
+              'key' => 'pria',
+              'value' => 'Pria',
+            ],
+            [
+              'key' => 'wanita',
+              'value' => 'Wanita',
+            ],
+          ],
+          'statusMarried' => [
+            [
+              'key' => 'belum_menikah',
+              'value' => 'Belum Menikah',
+            ],
+            [
+              'key' => 'sudah_menikah',
+              'value' => 'Sudah Menikah',
+            ],
+          ],
+          'statusActived' => [
+            [
+              'key' => 1,
+              'value' => 'Aktif',
+            ],
+          ],
+          'listMonth' => [
+            '01' => 'Januari',
+            '02' => 'Februari',
+            '03' => 'Maret',
+            '04' => 'April',
+            '05' => 'Mei',
+            '06' => 'Juni',
+            '07' => 'Juli',
+            '08' => 'Agustus',
+            '09' => 'September',
+            '10' => 'Oktober',
+            '11' => 'September',
+            '12' => 'Desember',
+          ],
+          'listYear' => [
+            '2017' => 2017,
+            '2018' => 2018,
+            '2019' => 2019,
+            '2020' => 2020,
+            '2021' => 2021,
+          ],
+        ],
     ],
 ];
